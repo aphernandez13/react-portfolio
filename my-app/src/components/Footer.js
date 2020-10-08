31352a0550a9be738b26b3b8b0dfe316bfedf942
alfredo pedroza <alfredopedrozahernandez13@gmail.com>
2020-10-07 23:42:00 -0700

    fixed footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,8 +3,8 @@ export default function Footer() {
   return (
     <div>
       <footer className="footer mt-auto py-3">
-        <div className="container">
-        <span className="text-align-center">Alfredo Pedroza, 2020</span>
+        <div className="container text-center">
+        <span className="text-muted">Alfredo Pedroza, 2020</span>
         </div>
       </footer>
     </div>
